a7a5243f1f7e2689f27060e83e5166fbe797e529
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-21 14:30:24 +0200

    create component DailyTask

---
+++ src/components/DailyTasks.js
@@ -0,0 +1,24 @@
+export default function DailyTasks() {
+  return (
+    <>
+      <h1>täglich</h1>
+      <p>
+        Deine tägliche Aufgaben. <br />
+        Schnell und mit wenigen Handgriffen erledigt.
+      </p>
+      <ul>
+        <li>Räume lüften</li>
+        <li>Betten machen</li>
+        <li>Spülmaschine ausräumen</li>
+        <li>Wäsche machen</li>
+        <li>Müll rausbringen</li>
+        <li>Böden saugen</li>
+        <li>ungenutzte Dinge wegräumen</li>
+        <li>Waschbecken im Bad reinigen</li>
+        <li>Toilette im Bad reinigen</li>
+        <li>Arbeitsflächen in der Küche abwischen</li>
+        <li>volle Spülmaschine anschalten</li>
+      </ul>
+    </>
+  );
+}
