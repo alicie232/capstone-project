953860af5933fdb482fc7ff2ade3728d8a55108f
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-21 12:11:43 +0200

    create list daily tasks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
 export default function App() {
-  return <div>App goes here</div>;
+  return (
+    <>
+      <h1>täglich</h1>
+      <p>
+        Deine tägliche Aufgaben. <br />
+        Schnell und mit wenigen Handgriffen erledigt.
+      </p>
+      <ul>
+        <li>Räume lüften</li>
+        <li>Betten machen</li>
+        <li>Spülmaschine ausräumen</li>
+        <li>Wäsche machen</li>
+        <li>Müll rausbringen</li>
+        <li>Böden saugen</li>
+        <li>ungenutzte Dinge wegräumen</li>
+        <li>Waschbecken im Bad reinigen</li>
+        <li>Toilette im Bad reinigen</li>
+        <li>Arbeitsflächen in der Küche abwischen</li>
+        <li>volle Spülmaschine anschalten</li>
+      </ul>
+    </>
+  );
 }
