47a76353bb4d6c476d4c3d297cbaf095ec34d7fb
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-25 20:41:32 +0200

    add checkbox for each task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,7 +2,10 @@ export default function TaskList({tasks}) {
   return (
     <ul>
       {tasks.map(task => (
-        <li key={task.id}>{task.task}</li>
+        <li key={task.id}>
+          <input type="checkbox" />
+          {task.task}
+        </li>
       ))}
     </ul>
   );
