febac7fe471f176ac58cd5a11154c5acf9a290a1
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-25 21:09:50 +0200

    add style for the list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,15 +2,15 @@ export default function TaskList({todos, setTodos}) {
   return (
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>
+        <li key={todo.id} style={{listStyle: 'none'}}>
           <input
             type="checkbox"
             checked={todo.isChecked}
             onChange={() => {
               setTodos(todos.map(todo_ => (todo_.id === todo.id ? {...todo_, isChecked: !todo.isChecked} : todo_)));
             }}
-          />
-          {todo.task}
+          />{' '}
+          <span style={{textDecoration: todo.isChecked && 'line-through'}}>{todo.task}</span>
         </li>
       ))}
     </ul>
