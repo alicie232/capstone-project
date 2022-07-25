b2871b2915767eaee9e07e539564cdc5f068c64c
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-25 21:35:23 +0200

    outsource dailyTasks to db.js

--- src/components/DailyTasks.js
+++ src/components/DailyTasks.js
@@ -1,22 +1,9 @@
 import {useState} from 'react';
-import {nanoid} from 'nanoid';
 import styled from 'styled-components';
 import TaskList from './TaskList';
+import dailyTasks from '../db';
 
 export default function DailyTasks() {
-  const dailyTasks = [
-    {task: 'Räume lüften', isChecked: false, id: nanoid()},
-    {task: 'Betten machen', isChecked: false, id: nanoid()},
-    {task: 'Spülmaschine ausräumen', isChecked: false, id: nanoid()},
-    {task: 'Wäsche machen', isChecked: false, id: nanoid()},
-    {task: 'Müll rausbringen', isChecked: false, id: nanoid()},
-    {task: 'Böden saugen', isChecked: false, id: nanoid()},
-    {task: 'ungenutzte Dinge wegräumen', isChecked: false, id: nanoid()},
-    {task: 'Waschbecken im Bad reinigen', isChecked: false, id: nanoid()},
-    {task: 'Toilette im Bad reinigen', isChecked: false, id: nanoid()},
-    {task: 'Arbeitsflächen in der Küche abwischen', isChecked: false, id: nanoid()},
-    {task: 'volle Spülmaschine anschalten', isChecked: false, id: nanoid()},
-  ];
   const [todos, setTodos] = useState(dailyTasks);
   return (
     <>
