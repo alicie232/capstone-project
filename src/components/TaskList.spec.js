23df928be7b7e28d25dde407ed5498320acff455
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-27 19:27:28 +0200

    downgrade nanoid to 3.3.4 and update test for TaskList

--- src/components/TaskList.spec.js
+++ src/components/TaskList.spec.js
@@ -1,11 +1,15 @@
 import {render, screen} from '@testing-library/react';
 import TaskList from './TaskList';
+import {nanoid} from 'nanoid';
 
-const dailyTasks = ['Räume lüften', 'Betten machen'];
+const dailyTasks = [
+  {task: 'Räume lüften', isChecked: false, checkedAt: '', id: nanoid()},
+  {task: 'Betten machen', isChecked: false, checkedAt: '', id: nanoid()},
+];
 
 describe('TaskList', () => {
   it('renders tasks', () => {
-    render(<TaskList tasks={dailyTasks} />);
+    render(<TaskList todos={dailyTasks} />);
 
     const task = screen.getByText(/Räume/i, /Betten/i);
 
