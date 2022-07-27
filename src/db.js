fca1a329737621b21e664f6a55ce0da30ac0523b
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-27 14:34:15 +0200

    add checktAt to dailyTasks

--- src/db.js
+++ src/db.js
@@ -1,16 +1,19 @@
 import {nanoid} from 'nanoid';
+
+let date = new Date();
+
 const dailyTasks = [
-  {task: 'Räume lüften', isChecked: false, id: nanoid()},
-  {task: 'Betten machen', isChecked: false, id: nanoid()},
-  {task: 'Spülmaschine ausräumen', isChecked: false, id: nanoid()},
-  {task: 'Wäsche machen', isChecked: false, id: nanoid()},
-  {task: 'Müll rausbringen', isChecked: false, id: nanoid()},
-  {task: 'Böden saugen', isChecked: false, id: nanoid()},
-  {task: 'ungenutzte Dinge wegräumen', isChecked: false, id: nanoid()},
-  {task: 'Waschbecken im Bad reinigen', isChecked: false, id: nanoid()},
-  {task: 'Toilette im Bad reinigen', isChecked: false, id: nanoid()},
-  {task: 'Arbeitsflächen in der Küche abwischen', isChecked: false, id: nanoid()},
-  {task: 'volle Spülmaschine anschalten', isChecked: false, id: nanoid()},
+  {task: 'Räume lüften', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Betten machen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Spülmaschine ausräumen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Wäsche machen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Müll rausbringen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Böden saugen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'ungenutzte Dinge wegräumen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Waschbecken im Bad reinigen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Toilette im Bad reinigen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'Arbeitsflächen in der Küche abwischen', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
+  {task: 'volle Spülmaschine anschalten', isChecked: false, checkedAt: date.toLocaleDateString(), id: nanoid()},
 ];
 
 export default dailyTasks;
