b11f34ec14cd2c2d3c9dfa0c15dd68e303570a57
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-28 17:04:18 +0200

    outsource HomePage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import styled from 'styled-components';
-import DailyTasks from './components/DailyTasks';
+import HomePage from './pages/HomePage';
 
 export default function App() {
-  return (
-    <>
-      <StyledHeader>
-        <h1>Aktuelle Aufgaben</h1>
-      </StyledHeader>
-      <Wrapper>
-        <DailyTasks />
-      </Wrapper>
-    </>
-  );
+  return <HomePage />;
 }
-
-const Wrapper = styled.div`
-  border: solid;
-  max-width: 375px;
-  padding: 10px;
-  margin: auto;
-`;
-
-const StyledHeader = styled.header`
-  text-align: center;
-`;
