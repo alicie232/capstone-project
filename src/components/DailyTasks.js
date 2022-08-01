c0ee81f9c7e186ecfa72eb2574aab8928064582b
Alice Cieslik <alice.cieslik@gmail.com>
2022-08-01 23:01:49 +0200

    outsource TaskList/add styling

--- src/components/DailyTasks.js
+++ src/components/DailyTasks.js
@@ -1,7 +1,8 @@
 import {useState, useEffect} from 'react';
 import {dailyTodos} from '../db';
-import styled from 'styled-components';
+
 import {loadFromLocalStorage, writeToLocalStorage} from '../util/localstorage';
+import TaskList from './TaskList';
 
 export default function DailyTasks() {
   const [todos, setTodos] = useState(() => {
@@ -21,7 +22,7 @@ export default function DailyTasks() {
     writeToLocalStorage('todos', todos);
   }, [todos]);
 
-  function handleChange(event) {
+  function handleTodos(event) {
     const todoId = event.target.dataset.todoid;
     const taskId = event.target.dataset.taskid;
 
@@ -48,46 +49,7 @@ export default function DailyTasks() {
 
   return (
     <>
-      {todos.map(todo => {
-        return (
-          <div key={todo.id}>
-            <h1>{todo.category}</h1>
-            <p>{todo.description}</p>
-            <ul>
-              {todo.tasks.map(task => {
-                return (
-                  <ListItem key={task.id}>
-                    <label>
-                      <Checkbox
-                        type="checkbox"
-                        checked={task.isChecked}
-                        data-todoid={todo.id}
-                        data-taskid={task.id}
-                        onChange={handleChange}
-                      />
-                      <Task checked={task.isChecked}>{task.task}</Task>
-                    </label>
-                  </ListItem>
-                );
-              })}
-            </ul>
-          </div>
-        );
-      })}
+      <TaskList todos={todos} onTodoChange={handleTodos} />
     </>
   );
 }
-
-const ListItem = styled.li`
-  list-style: none;
-`;
-
-const Task = styled.span`
-  text-decoration: ${({checked}) => {
-    return checked && 'line-through';
-  }};
-`;
-
-const Checkbox = styled.input`
-  accent-color: #9b59b6;
-`;
---
+++ src/components/TaskList.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export default function TaskList({todos, onTodoChange}) {
+  return todos.map(todo => {
+    return (
+      <Wrapper key={todo.id}>
+        <h1>{todo.category}</h1>
+        <p>{todo.description}</p>
+        <StyledList>
+          {todo.tasks.map(task => {
+            return (
+              <ListItem key={task.id}>
+                <label>
+                  <Checkbox
+                    type="checkbox"
+                    checked={task.isChecked}
+                    data-todoid={todo.id}
+                    data-taskid={task.id}
+                    onChange={onTodoChange}
+                  />
+                  <Task checked={task.isChecked}>{task.task}</Task>
+                </label>
+              </ListItem>
+            );
+          })}
+        </StyledList>
+      </Wrapper>
+    );
+  });
+}
+
+const Wrapper = styled.div`
+  border: solid;
+  border-radius: 15px;
+  margin: 5px;
+  padding: 5px;
+`;
+
+const StyledList = styled.ul`
+  padding: 0 0 0 15px;
+`;
+
+const ListItem = styled.li`
+  list-style: none;
+`;
+
+const Checkbox = styled.input`
+  accent-color: #9b59b6;
+`;
+
+const Task = styled.span`
+  text-decoration: ${({checked}) => {
+    return checked && 'line-through';
+  }};
+`;
