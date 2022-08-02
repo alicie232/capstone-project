5d03ff577cfa7a7624ec16f166d8daa84357440f
Alice Cieslik <alice.cieslik@gmail.com>
2022-08-02 22:18:06 +0200

    filter tasks by weekday

--- src/components/DailyTasks.js
+++ src/components/DailyTasks.js
@@ -1,10 +1,36 @@
 import {useState, useEffect} from 'react';
 import {dailyTodos} from '../db';
-
 import {loadFromLocalStorage, writeToLocalStorage} from '../util/localstorage';
 import TaskList from './TaskList';
 
 export default function DailyTasks() {
+  const [daily, monday, tuesday, wednesday, thursday, friday, weekend] = dailyTodos;
+  let currentday;
+  switch (new Date().getDay()) {
+    case 0:
+      currentday = [weekend];
+      break;
+    case 1:
+      currentday = [daily, monday];
+      break;
+    case 2:
+      currentday = [daily, tuesday];
+      break;
+    case 3:
+      currentday = [daily, wednesday];
+      break;
+    case 4:
+      currentday = [daily, thursday];
+      break;
+    case 5:
+      currentday = [daily, friday];
+      break;
+    case 6:
+      currentday = [weekend];
+      break;
+    default:
+  }
+
   const [todos, setTodos] = useState(() => {
     const todosFromLocal = loadFromLocalStorage('todos');
     const uncheckedTodos = todosFromLocal?.map(todo => {
@@ -18,7 +44,7 @@ export default function DailyTasks() {
       }
       return todo;
     });
-    return uncheckedTodos ?? dailyTodos;
+    return uncheckedTodos ?? currentday;
   });
 
   useEffect(() => {
